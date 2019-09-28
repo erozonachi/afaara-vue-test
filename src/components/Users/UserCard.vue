088ee7642615bf286149e11c9b8fc2d0a88e576c
James Eneh <erozonachi.james@gmail.com>
2019-09-28 23:43:12 +0100

    Create a user card component

---
+++ src/components/Users/UserCard.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="user-card">
+    <div class="content">
+      <span class="avatar">{{user.name.charAt(0)}}</span>
+      <h3>{{user.name}}</h3>
+    </div>
+    <div class="content">
+      <div class="user-info">
+        <img src alt="Phone:" />
+        <span>{{user.phone}}</span>
+      </div>
+      <div class="user-info">
+        <img src alt="Email:" />
+        <span>{{user.email}}</span>
+      </div>
+      <div class="user-info">
+        <img src alt="City:" />
+        <span>{{user.address.city}}</span>
+      </div>
+      <div class="user-info">
+        <img src alt="Website:" />
+        <span>{{user.website}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserCard",
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.user-card {
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  max-width: 800px;
+  width: 100%;
+  padding: 10px;
+}
+.content {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+  width: 100%;
+}
+.avatar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  box-sizing: border-box;
+  background-color: #4265ed;
+  color: #dddddd;
+  font-size: 32px;
+  font-weight: bold;
+}
+h3 {
+  font-size: 120%;
+  font-weight: 600;
+  color: #4f4f4f;
+  text-transform: capitalize;
+}
+.user-info {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  box-sizing: border-box;
+  width: 49%;
+}
+.user-info img {
+  margin: 0 10px;
+  color: #333333;
+  font-weight: bold;
+}
+.user-info span {
+  font-size: 100%;
+}
+</style>
