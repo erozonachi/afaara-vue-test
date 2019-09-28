169349739e27b7bc0b61a3aa7bb1b41a077d6dc7
James Eneh <erozonachi.james@gmail.com>
2019-09-29 00:09:13 +0100

    Create a user list component

---
+++ src/components/Users/UserList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="users-container" v-if="users.length">
+    <UserCard v-for="user of users" :user="user" :key="user.id" />
+  </div>
+  <div class="users-container" v-else>No user found</div>
+</template>
+
+<script>
+import UserCard from "./UserCard.vue";
+
+export default {
+  name: "UserList",
+  components: {
+    UserCard
+  },
+  data: () => ({
+    users: []
+  })
+};
+</script>
+
+<style scoped>
+.users-container {
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 100%;
+}
+</style>
