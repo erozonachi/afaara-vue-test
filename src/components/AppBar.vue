61b8e5e1268af7b15fd1e55afa3308fc7275afc3
James Eneh <erozonachi.james@gmail.com>
2019-09-29 12:49:03 +0100

    Create and export app bar component

---
+++ src/components/AppBar.vue
@@ -0,0 +1,69 @@
+<template>
+  <nav>
+    <div>
+      A
+      <span></span>
+    </div>
+    <div>
+      F
+      <span></span>
+    </div>
+    <div>
+      A
+      <span></span>
+    </div>
+    <div>
+      A
+      <span></span>
+    </div>
+    <div>
+      R
+      <span></span>
+    </div>
+    <div>
+      A
+      <span></span>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "AppBar"
+};
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 60px;
+  background-color: #4265ed;
+}
+nav > div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  background-color: rgba(255, 2555, 255, 0.8);
+  color: #4265ed;
+  font-size: 28px;
+  font-weight: bold;
+}
+nav > div > span {
+  position: absolute;
+  right: 10px;
+  bottom: 10px;
+  width: 10px;
+  height: 10px;
+  border-radius: 5px;
+  background-color: #4265ed;
+}
+</style>
