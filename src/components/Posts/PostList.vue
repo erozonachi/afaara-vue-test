a0b79bd71b92cbf008425a1f6bcade3af4f93eeb
James Eneh <erozonachi.james@gmail.com>
2019-09-29 03:11:02 +0100

    Create and export post list component

---
+++ src/components/Posts/PostList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="posts-container" v-if="posts.length">
+    <PostCard v-for="post of posts" :post="post" :key="post.id" />
+  </div>
+  <div class="posts-container" v-else>No post found</div>
+</template>
+
+<script>
+import PostCard from "./PostCard.vue";
+
+export default {
+  name: "PostList",
+  components: {
+    PostCard
+  },
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    },
+    fetch: {
+      type: Function,
+      required: true
+    }
+  },
+  created: async function() {
+    await this.fetch();
+  }
+};
+</script>
+
+<style scoped>
+.posts-container {
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 100%;
+}
+</style>
