3286ac272e7c4dbabc7383758ef2374d29e851ea
James Eneh <erozonachi.james@gmail.com>
2019-09-29 13:30:34 +0100

    User loader in post list component

--- src/components/Posts/PostList.vue
+++ src/components/Posts/PostList.vue
@@ -2,33 +2,39 @@
   <div class="posts-container" v-if="posts.length">
     <PostCard v-for="post of posts" :post="post" :key="post.id" />
   </div>
+  <Loader v-else-if="isFetching" />
   <div class="posts-container" v-else>No post found</div>
 </template>
 
 <script>
 import PostCard from "./PostCard.vue";
+import Loader from "../Loader.vue";
 import axios from "axios";
 
 export default {
   name: "PostList",
   components: {
-    PostCard
+    PostCard,
+    Loader
   },
 
   data: () => ({
-    posts: []
+    posts: [],
+    isFetching: false
   }),
   methods: {
     fetchPosts: async function() {
       try {
+        this.isFetching = true;
         const { data } = await axios.get(
           `https://jsonplaceholder.typicode.com/users/${this.$route.params.id}/posts`
         );
         this.posts = data.filter(
           item => item.userId === parseInt(this.$route.params.id)
         );
+        this.isFetching = false;
       } catch (error) {
-        //console.log(error);
+        this.isFetching = false;
       }
     }
   },
