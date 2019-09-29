b759a4f9dd521f45828ff9d3cbd1173b171e5758
James Eneh <erozonachi.james@gmail.com>
2019-09-29 15:51:07 +0100

    Create and export post mixin module

--- src/components/Posts/PostList.vue
+++ src/components/Posts/PostList.vue
@@ -8,42 +8,10 @@
 </template>
 
 <script>
-import PostCard from "./PostCard.vue";
-import Loader from "../Loader.vue";
-import Navigator from "../Navigator.vue";
 import axios from "axios";
 
 export default {
-  name: "PostList",
-  components: {
-    PostCard,
-    Loader,
-    Navigator
-  },
-
-  data: () => ({
-    posts: [],
-    isFetching: false
-  }),
-  methods: {
-    fetchPosts: async function() {
-      try {
-        this.isFetching = true;
-        const { data } = await axios.get(
-          `https://jsonplaceholder.typicode.com/users/${this.$route.params.id}/posts`
-        );
-        this.posts = data.filter(
-          item => item.userId === parseInt(this.$route.params.id)
-        );
-        this.isFetching = false;
-      } catch (error) {
-        this.isFetching = false;
-      }
-    }
-  },
-  created: async function() {
-    await this.fetchPosts();
-  }
+  name: "PostList"
 };
 </script>
 
